68712707619e4987a186b5693a14ca94d4312899
Logan Bonney <wakeup2early@gmail.com>
2017-10-05 15:25:18 -0600

    added layout for MLP.py

---
+++ MLP.py
@@ -0,0 +1,20 @@
+import node 
+
+class MLP:
+
+    input_layer = []
+    hidden_layers = []
+    output_layer = []
+    momentum = None
+    converged = False
+
+    def __init__(num_inputs, num_layers, nodes_per_layer, num_output, momentum):
+
+    def train(training_data):
+          
+    def hypothesis_of(testing_data):
+
+    def calculate_error():
+    
+    def output_results():
+
