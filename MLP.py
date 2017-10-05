80996d1fbbb0de5f66760f97bcf5c74d87358540
BradWhite <white.brad17@gmail.com>
2017-10-05 15:46:38 -0600

    Formatting

--- MLP.py
+++ MLP.py
@@ -8,13 +8,18 @@ class MLP:
     momentum = None
     converged = False
 
-    def __init__(num_inputs, num_layers, nodes_per_layer, num_output, momentum):
+    def __init__(self, num_inputs, num_layers, nodes_per_layer, num_output, momentum):
+        pass
 
-    def train(training_data):
+    def train(self, training_data):
+        pass
           
-    def hypothesis_of(testing_data):
+    def hypothesis_of(self, testing_data):
+        pass
 
-    def calculate_error():
+    def calculate_error(self):
+        pass
     
-    def output_results():
+    def output_results(self):
+        pass
 
