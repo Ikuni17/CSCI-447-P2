b2349734de653a90551ae3fbae10f4dc6db1a58f
Trent <b.trent5@gmail.com>
2017-10-06 15:33:06 -0600

    added actual rosenbrock math

--- rosen_generator.py
+++ rosen_generator.py
@@ -3,34 +3,37 @@
 # with a - d being inputs and each x being the associated output.
 
 import sys
+import random
 
 num_inputs = int(sys.argv[1])
 num_data_points = int(sys.argv[2])
 output_file_address = str(sys.argv[3])
 output_file = open(output_file_address, 'a')
 
-
 def rosen(inputs):
-    return sum(list(inputs))
+	totals = []
+	for i in range(len(inputs)):
+		if i < (len(inputs)-1):
+			temp = (((1 - inputs[i])**2) + (100 * (inputs[i+1] - inputs[i]**2)**2))
+			totals.append(temp)
+	result = sum(totals)
+	print(str(result))
 
+def input_gen(type, dimensions, num_data_points):
+	inputs = []
+	next_input = []
+	if type == 0:
+		for i in range(num_data_points):
+			next_input = []
+			for j in range(dimensions):
+				next_input.append(random.random() * num_data_points)
+# 			print(str(i) + ': ' + str(next_input) + '\n')
+	elif type == 1:
+		pass
+	elif type == 2:
+		pass
+	output_file.write(str(inputs))
 
-def input_gen(type, dimensions):
-    inputs = []
-    # 	use 1 for sequential inputs
-    if int(type) == 1:
-        for i in range(num):
-            inputs.append(i)
-    # 	use 2 for random inputs
-    elif int(type) == 2:
-        pass
-    # 	use 3 for grid
-    # 	should find out what grid is
-    elif int(type) == 3:
-        pass
-    return inputs
-
-
-    for i in range(num_data_points):
-        output_file.write(str(input_gen(1, dimensions)))
-
+output_file.write(str(rosen([0,1,8])))
+# output_file.write(str(input_gen(0, 2, num_data_points)))
 output_file.close()
