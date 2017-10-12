9f4759a7b1ad423b151aeeb4742ba0d7a9af5b87
Trent <b.trent5@gmail.com>
2017-10-12 17:52:42 -0600

    updated random and seq. started grid

--- rosen_generator.py
+++ rosen_generator.py
@@ -10,15 +10,17 @@
 
 import sys
 import random
+import itertools
+import math
 
 # takes a list of inputs and returns the rosenbrock function output on those inputs
 def rosen(inputs):
-    totals = []
-    for i in range(len(inputs) - 1):
-        temp = (((1 - inputs[i]) ** 2) + (100 * (inputs[i + 1] - inputs[i] ** 2) ** 2))
-        totals.append(temp)
-    result = sum(totals)
-    return result
+	totals = []
+	for i in range(len(inputs) - 1):
+		temp = (((1 - inputs[i]) ** 2) + (100 * (inputs[i + 1] - inputs[i] ** 2) ** 2))
+		totals.append(temp)
+	result = sum(totals)
+	return result
 
 
 # generates inputs for rosenbrock function
@@ -28,31 +30,48 @@ def rosen(inputs):
 def input_gen(type, dimensions, num_data_points):
 	generated = []
 	if type == 0:
+		# random numbers
 		for i in range(num_data_points):
 			temp = []
 			for j in range(dimensions):
 				temp.append((random.random() * 2) - 1)
 			generated.append(temp)
 	elif type == 1:
-		counter = 1
+		# sequential numbers
+		step = (2 / (dimensions * num_data_points))
+		counter = (-1.0 + (step/2))
 		for i in range(num_data_points):
 			temp = []
 			for j in range(dimensions):
 				temp.append(counter)
-				counter += 1
+				counter += step
 			generated.append(temp)
 	elif type == 2:
-		pass
+		# grid numbers
+		# NOT YET FUNCTIONAL
+		grid = []
+		vals = []
+		step = (2 / (dimensions * num_data_points))
+		counter = (-1.0 + (step/2))
+		# creates a list of values that is num_data_points long
+		for i in range(num_data_points):
+			counter += step
+			vals.append(counter)
+
+		possible = itertools.combinations(random.shuffle(vals), dimensions)
+		for i in range(num_data_points):
+			print(str(list(possible)))
+
 	return generated
 
 # appends the result of the rosenbrock function to each input and records the result in output_file
 def apply_rosen(inputs):
-    for i in inputs:
-        dimension = len(i)
-        i.append(rosen(i))
-        if __name__ == '__main__':
-            output_file.write(str(i) + '\n')
-    return inputs
+	for i in inputs:
+		dimension = len(i)
+		i.append(rosen(i))
+		if __name__ == '__main__':
+			output_file.write(str(i) + '\n')
+	return inputs
 
 
 # generates a data set based on the provided parameters
@@ -69,7 +88,7 @@ if __name__ == "__main__":
 	dimensions = int(sys.argv[2])
 	num_data_points = int(sys.argv[3])
 	output_file_address = str(sys.argv[4])
-	output_file = open(output_file_address, 'a')
+	output_file = open(output_file_address, 'w')
 
 	generate(input_type, dimensions, num_data_points)
 
