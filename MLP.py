7bc222c8608d3387b0579670cece4cf776ae0e67
BradWhite <white.brad17@gmail.com>
2017-10-12 16:50:05 -0600

    Added weight matrix initialization

--- MLP.py
+++ MLP.py
@@ -1,31 +1,50 @@
-import node 
+import node
+import random
 
 class MLP:
-
+    # List of input node objects
     input_layer = []
+    # List of lists which contain nodes for a hidden layer
     hidden_layers = [[]]
+    # List of output nodes
     output_layer = []
     momentum = None
     converged = False
+    # Matrix of weights where the connect between nodes i and j will be at index [i][j]. The third list contains
+    # historical weights for that connection, where the 0th index is the current weight.
     weights = [[[]]]
 
     def __init__(self, num_inputs, num_layers, nodes_per_layer, num_outputs, momentum):
         node_id_counter = 0
 
+        # Initialize input layer
         for i in range(num_inputs):
             self.input_layer.append(node.node(node_id_counter, True, 0))
+            self.weights
             node_id_counter += 1
 
-        for j in range(num_layers):
-            if j != 0:
+        # Initialize any hidden layers
+        for i in range(num_layers):
+            if i != 0:
                 self.hidden_layers.append([])
-            for k in range(nodes_per_layer[j]):
-                self.hidden_layers[j].append(node.node(node_id_counter, True, 0))
+            for j in range(nodes_per_layer[i]):
+                self.hidden_layers[i].append(node.node(node_id_counter, True, 0))
                 node_id_counter += 1
 
-        for m in range(num_outputs):
+        # Initialize output layer
+        for i in range(num_outputs):
             self.output_layer.append(node.node(node_id_counter, True, 0))
             node_id_counter += 1
+
+        # Initialize all weights to zero
+        self.weights = [[[0] for i in range(node_id_counter)] for j in range(node_id_counter)]
+        # Add a random weight between 0 and 1 if it isn't the connection to itself
+        for i in range(node_id_counter):
+            for j in range(node_id_counter):
+                if i == j:
+                    continue
+                else:
+                    self.weights[i][j][0] = random.random()
 
     def train(self, training_data):
         # Train the first layer from the input layer
@@ -62,7 +81,7 @@ class MLP:
             print("{0}".format(self.output_layer[i].number), end=', ')
 
 def main():
-    mlp_network = MLP(2, 2, [4, 6], 1, 0)
+    mlp_network = MLP(5, 2, [5, 5], 1, 0)
     mlp_network.print_network()
 
 main()
