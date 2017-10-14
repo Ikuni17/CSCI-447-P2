f6aabee004e4a1f550771ed72c6eef9fb7fe1227
BradWhite <white.brad17@gmail.com>
2017-10-14 15:17:04 -0600

    Formatting

--- RBF.py
+++ RBF.py
@@ -3,77 +3,80 @@ import math
 import numpy as np
 from matplotlib import pyplot as plt
 
+
 class RBF:
-	sigma = 0.1
-	def __init__(self, num_basis, train_in):
-		self.train_in = []
-		self.train_out = []
-		self.weights = []
-		self.centers = []
-		self.sigmas = []
-		#print(train_in)
-		for x in train_in:
-			self.train_in.append(x[:len(x)-1])
-			self.train_out.append(x[len(x)-1])
+    sigma = 0.1
 
-		# initialize weight and centers array with random values
-		for i in range(num_basis):
-			# Put random point from training data as center
-			self.centers.append(self.train_in[random.randint(0, len(self.train_in) - 1)])
-			self.weights.append(random.uniform(0,100))
-			self.sigmas.append(random.uniform(0, 0.3))
+    def __init__(self, num_basis, train_in):
+        self.train_in = []
+        self.train_out = []
+        self.weights = []
+        self.centers = []
+        self.sigmas = []
+        # print(train_in)
+        for x in train_in:
+            self.train_in.append(x[:len(x) - 1])
+            self.train_out.append(x[len(x) - 1])
 
-	def train(self):
-		out = self.gradient_descent(RBF.get_outputs(self.train_in, self.weights, self.centers), self.train_out, self.weights, 0.001, 100000)
-		self.weights = out[0]
-		plt.plot(out[1])
-		plt.ylabel('cost')
-		plt.axis([0, max(out[1]), 0, len(out[1])]) 
-		plt.show()
-		#print(self.weights)
-		output = self.hypothesis(self.train_in)
-		#for i in range(len(output)):
-			#print('Expected out: {0}, Actual out: {1}'.format(self.train_out[i], output[i]))
+        # initialize weight and centers array with random values
+        for i in range(num_basis):
+            # Put random point from training data as center
+            self.centers.append(self.train_in[random.randint(0, len(self.train_in) - 1)])
+            self.weights.append(random.uniform(0, 100))
+            self.sigmas.append(random.uniform(0, 0.3))
 
-	
-	def hypothesis(self, data_in):
-		output = []
-		for datapoint in data_in:
-			value = 0
-			for i in range(len(self.weights)):
-				value += self.weights[i]*RBF.gaussian_function(datapoint, self.centers[i], RBF.sigma)
-			output.append(value)
-		return output
+    def train(self):
+        out = self.gradient_descent(RBF.get_outputs(self.train_in, self.weights, self.centers), self.train_out,
+                                    self.weights, 0.001, 100000)
+        self.weights = out[0]
+        plt.plot(out[1])
+        plt.ylabel('cost')
+        plt.axis([0, max(out[1]), 0, len(out[1])])
+        plt.show()
+        # print(self.weights)
+        output = self.hypothesis(self.train_in)
 
-	@staticmethod
-	def gaussian_function(datapoint, center, sigma):
-		dist = np.linalg.norm(np.array(datapoint) - np.array(center))
-		return math.exp(-(dist**2)/2*sigma**2)
+        # for i in range(len(output)):
+        # print('Expected out: {0}, Actual out: {1}'.format(self.train_out[i], output[i]))
 
-	@staticmethod
-	def get_outputs(data_in, weights, centers):
-		output = []
-		for datapoint in data_in:
-			value = []
-			for i in range(len(weights)):
-				value.append(RBF.gaussian_function(datapoint, centers[i], RBF.sigma))
-			output.append(value)
-		return output
+    def hypothesis(self, data_in):
+        output = []
+        for datapoint in data_in:
+            value = 0
+            for i in range(len(self.weights)):
+                value += self.weights[i] * RBF.gaussian_function(datapoint, self.centers[i], RBF.sigma)
+            output.append(value)
+        return output
 
-	@staticmethod
-	def gradient_descent(inputs, output, weights, alpha, num_iter):
-		y = np.array(output)
-		theta = np.array(weights)
-		x = np.array(inputs)
-		#print(theta)
-		x_trans = x.transpose()
-		costs = []
-		for i in range(num_iter):
-			hypothesis = np.dot(x, theta)
-			# print(hypothesis[2])
-			loss = hypothesis - y
-			cost = np.sum(loss ** 2)
-			costs.append(cost)
-			gradient = np.dot(x_trans,loss)/(np.shape(loss))
-			theta = theta - alpha*gradient
-		return (theta.tolist(), costs)
+    @staticmethod
+    def gaussian_function(datapoint, center, sigma):
+        dist = np.linalg.norm(np.array(datapoint) - np.array(center))
+        return math.exp(-(dist ** 2) / 2 * sigma ** 2)
+
+    @staticmethod
+    def get_outputs(data_in, weights, centers):
+        output = []
+        for datapoint in data_in:
+            value = []
+            for i in range(len(weights)):
+                value.append(RBF.gaussian_function(datapoint, centers[i], RBF.sigma))
+            output.append(value)
+        return output
+
+    @staticmethod
+    def gradient_descent(inputs, output, weights, alpha, num_iter):
+        y = np.array(output)
+        theta = np.array(weights)
+        x = np.array(inputs)
+        # print(theta)
+        x_trans = x.transpose()
+        costs = []
+        for i in range(num_iter):
+            hypothesis = np.dot(x, theta)
+            # print(hypothesis[2])
+            loss = hypothesis - y
+            cost = np.sum(loss ** 2)
+            costs.append(cost)
+            gradient = np.dot(x_trans, loss) / (np.shape(loss))
+            theta = theta - alpha * gradient
+        return (theta.tolist(), costs)
